9b0da8be7987ef7f046c9bde4af9c21baa60784b
arsh <11322brar@gmail.com>
2024-09-22 21:48:14 +0530

    update 'Leetcode Metrics/src/index.html' and output.css

---
+++ Countdown timer/src/script.js
@@ -0,0 +1,22 @@
+const festivals = [
+    { festivalName: "New Year", date: "01 January 2025" },
+    { festivalName: "Lohri", date: "13 January 2025" },
+    { festivalName: "Makar Sankranti", date: "14 January 2025" },
+    { festivalName: "Republic Day", date: "26 January 2025" },
+    { festivalName: "Maha Shivaratri", date: "26 February 2025" },
+    { festivalName: "Holi", date: "14 March 2025" },
+    { festivalName: "Eid al-Fitr", date: "02 April 2025" },
+    { festivalName: "Good Friday", date: "18 April 2025" },
+    { festivalName: "Raksha Bandhan", date: "09 August 2025" },
+    { festivalName: "Ganesh Chaturthi", date: "28 August 2025" },
+    { festivalName: "Dussehra", date: "02 October 2025" },
+    { festivalName: "Diwali", date: "29 October 2025" },
+    { festivalName: "Christmas", date: "25 December 2025" }
+];
+
+
+
+
+
+
+
